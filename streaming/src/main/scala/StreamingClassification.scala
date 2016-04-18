0fbfcde1a671ff8c0e57fb6cd694fa2af1b48dc9
Mitchell Tracy <mittracy7@gmail.com>
2016-04-18 00:07:55 -0400

    can parse class from streaming csv

--- streaming/src/main/scala/StreamingClassification.scala
+++ streaming/src/main/scala/StreamingClassification.scala
@@ -7,10 +7,48 @@ import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.SparkConf
 
-object StreamingClassification {
 
+
+	
+object StreamingClassification {	
+	
+	
+	class Student(
+	val sex: Int,
+	val age: Int,
+	val Medu: Int,
+	val Fedu: Int,
+	val travel: Int,
+	val study: Int,
+	val failures: Int,
+	val schoolsup: Int,
+	val famsup: Int,
+	val paid: Int,
+	val activities: Int,
+	val nursery: Int,
+	val higher: Int,
+	val internet: Int,
+	val romantic: Int,
+	val famrel: Int,
+	val freetime: Int,
+	val goout: Int,
+	val Dalc: Int,
+	val Walc: Int,
+	val health: Int,
+	val absences: Int,
+	val G1: Int,
+	val G2: Int,
+	val G3: Double){
+		
+		def printPassed() = {
+			println(G3)
+		}
+		
+	}
 
     def main(args: Array[String]) {
+		Logger.getLogger("org").setLevel(Level.OFF);
+		Logger.getLogger("akka").setLevel(Level.OFF);
 		val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
 		val ssc = new StreamingContext(sparkConf, Seconds(2))
 		// Create direct kafka stream with brokers and topics
@@ -19,11 +57,56 @@ object StreamingClassification {
 		val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
 			ssc, kafkaParams, topicsSet)
 
-    // Get the lines, split them into words, count the words and print
+
+        
+        /*
+        
+        val customSchema = StructType(Array(
+			StructField("sex", IntegerType, true),
+			StructField("age", IntegerType, true),
+			StructField("Medu", IntegerType, true),
+			StructField("Fedu", IntegerType, true),
+			StructField("traveltime", IntegerType, true),
+			StructField("studytime", IntegerType, true),
+			StructField("failures", IntegerType, true),
+			StructField("schoolsup", IntegerType, true),
+			StructField("famsup", IntegerType, true),
+			StructField("paid", IntegerType, true),
+			StructField("activities", IntegerType, true),
+			StructField("nursery", IntegerType, true),
+			StructField("higher", IntegerType, true),
+			StructField("internet", IntegerType, true),
+			StructField("romantic", IntegerType, true),
+			StructField("famrel", IntegerType, true),
+			StructField("freetime", IntegerType, true),
+			StructField("goout", IntegerType, true),
+			StructField("Dalc", IntegerType, true),
+			StructField("Walc", IntegerType, true),
+			StructField("health", IntegerType, true),
+			StructField("absences", IntegerType, true),
+			StructField("G1", IntegerType, true),
+			StructField("G2", IntegerType, true),
+			StructField("G3", DoubleType, true)))
+			* 
+			* */
+
+			
+			
+		
+		    // Get the lines, split them into words, count the words and print
 		val lines = messages.map(_._2)
-		val words = lines.flatMap(_.split(" "))
-		val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-			wordCounts.print()
+		val s = lines.map{ k => 
+			val arr = k.split(",")
+			new Student(arr(0).toInt, arr(1).toInt, arr(2).toInt, arr(3).toInt, arr(4).toInt, arr(5).toInt,
+					arr(6).toInt, arr(7).toInt, arr(8).toInt, arr(9).toInt, arr(10).toInt,
+					arr(11).toInt, arr(12).toInt, arr(13).toInt, arr(14).toInt, arr(15).toInt,
+					arr(16).toInt, arr(17).toInt, arr(18).toInt, arr(19).toInt, arr(20).toInt,
+					arr(21).toInt, arr(22).toInt, arr(23).toInt, arr(24).toDouble)
+		}
+		s.foreachRDD{ k =>
+			k.foreach{s => s.printPassed()}
+		}
+		
 
     // Start the computation
 		ssc.start()
